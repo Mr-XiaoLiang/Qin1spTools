833e1dafe359dbc136751c6b78e3b735b8211eb7
lollipop <liujian594@hotmail.com>
2021-08-25 22:39:34 +0800

    增加引导工具

--- app/src/main/java/com/lollipop/qin1sptools/guide/Guide.kt
+++ app/src/main/java/com/lollipop/qin1sptools/guide/Guide.kt
@@ -1,11 +1,133 @@
 package com.lollipop.qin1sptools.guide
 
+import android.app.Activity
+import android.graphics.Color
+import android.view.Gravity
+import android.view.ViewGroup
+import android.view.ViewManager
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.lollipop.qin1sptools.activity.base.BaseActivity
+import com.lollipop.qin1sptools.event.KeyEventProvider
+import com.lollipop.qin1sptools.utils.dp2px
+import javax.microedition.util.LinkedList
+import kotlin.math.acos
+
 /**
  * @author lollipop
  * @date 2021/8/25 22:15
  */
-class Guide {
+class Guide private constructor(private val option: Option) {
 
+    companion object {
 
+        private const val VIEW_INSETS_PADDING_DP = 10
+
+        fun create(activity: BaseActivity): Builder {
+            return Builder(activity, activity)
+        }
+    }
+
+    private val guideTextView: TextView by lazy {
+        TextView(option.activity).apply {
+            gravity = Gravity.CENTER
+            setTextColor(Color.WHITE)
+        }
+    }
+
+    private val guideSketchView: ImageView by lazy {
+        ImageView(option.activity).apply {
+            scaleType = ImageView.ScaleType.CENTER_INSIDE
+        }
+    }
+
+    private val guideView: LinearLayout by lazy {
+        LinearLayout(option.activity).apply {
+            orientation = LinearLayout.VERTICAL
+            val padding = VIEW_INSETS_PADDING_DP.dp2px().toInt()
+            setPadding(padding, padding, padding, padding)
+            addView(guideTextView, createLayoutParams())
+            addView(guideSketchView, createLayoutParams().apply {
+                setMargins(0, padding, 0, 0)
+            })
+        }
+    }
+
+    private fun show() {
+        if (!initView()) {
+            return
+        }
+        // TODO
+    }
+
+    private fun initView(): Boolean {
+        val guideContainer = option.activity.window.decorView
+        if (guideContainer !is ViewGroup) {
+            return false
+        }
+        guideView.parent?.let {
+            if (it != guideContainer) {
+                if (it is ViewManager) {
+                    it.removeView(guideView)
+                } else {
+                    return false
+                }
+            } else {
+                return true
+            }
+        }
+        guideContainer.addView(
+            guideView,
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+        return true
+    }
+
+    private fun nextStep() {
+        // TODO
+    }
+
+    private fun createLayoutParams(): LinearLayout.LayoutParams {
+        return LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT, 0
+        ).apply {
+            weight = 1F
+        }
+    }
+
+    private class Option(
+        val activity: Activity,
+        val keyEventProvider: KeyEventProvider,
+        val stepList: LinkedList<Step>
+    )
+
+    class Builder(
+        private val activity: Activity,
+        private val keyEventProvider: KeyEventProvider
+    ) {
+
+        private val stepList = LinkedList<Step>()
+
+        fun next(step: Step): Builder {
+            stepList.addLast(step)
+            return this
+        }
+
+        fun show() {
+            if (stepList.isEmpty) {
+                return
+            }
+            Guide(
+                Option(
+                    activity,
+                    keyEventProvider,
+                    stepList
+                )
+            ).show()
+        }
+
+    }
 
 }
---
+++ app/src/main/java/com/lollipop/qin1sptools/guide/Step.kt
@@ -0,0 +1,14 @@
+package com.lollipop.qin1sptools.guide
+
+/**
+ * @author lollipop
+ * @date 2021/8/25 22:17
+ */
+class Step(
+    val msg: CharSequence,
+    val sketchId: Int = 0
+) {
+
+
+
+}
