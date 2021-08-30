8f1692cc047b9a72e262728d363c161633f32ba0
lollipop <liujian594@hotmail.com>
2021-08-30 20:39:37 +0800

    增加任务管理工具类

---
+++ app/src/main/java/com/lollipop/qin1sptools/task/AppInfo.kt
@@ -0,0 +1,9 @@
+package com.lollipop.qin1sptools.task
+
+/**
+ * @author lollipop
+ * @date 2021/8/30 20:36
+ */
+class AppInfo(
+
+)
---
+++ app/src/main/java/com/lollipop/qin1sptools/task/AppTaskManager.kt
@@ -0,0 +1,19 @@
+package com.lollipop.qin1sptools.task
+
+import android.content.Context
+
+/**
+ * @author lollipop
+ * @date 2021/8/30 20:35
+ */
+class AppTaskManager {
+
+    val appList = ArrayList<AppInfo>()
+
+    val runningTaskList = ArrayList<TaskInfo>()
+
+    fun refresh(context: Context) {
+
+    }
+
+}
