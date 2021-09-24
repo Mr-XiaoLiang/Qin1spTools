e82dca1b68b8edd3c71ffc5e79536b535b575b32
lollipop <liujian594@hotmail.com>
2021-09-24 22:47:54 +0800

    增加宫格图标选中边框

--- app/src/main/java/com/lollipop/qin1sptools/view/GridSelectedFrameView.kt
+++ app/src/main/java/com/lollipop/qin1sptools/view/GridSelectedFrameView.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.graphics.*
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
+import kotlin.math.max
+import kotlin.math.min
 
 /**
  * @author lollipop
@@ -15,6 +17,12 @@ class GridSelectedFrameView(
 
     constructor(context: Context, attr: AttributeSet?) : this(context, attr, 0)
     constructor(context: Context) : this(context, null)
+
+    private val selectedFrameDrawable = SelectedFrameDrawable()
+
+    init {
+        setImageDrawable(selectedFrameDrawable)
+    }
 
     private class SelectedFrameDrawable : Drawable() {
 
@@ -54,19 +62,44 @@ class GridSelectedFrameView(
 
         private fun checkPath() {
             framePath.reset()
-            TODO("Not yet implemented")
+            if (bounds.isEmpty) {
+                return
+            }
+            val offset = strokeWidth / 2
+            val radiusHalf = radius / 2
+            val left = bounds.left + offset
+            val top = bounds.top + offset
+            val right = bounds.right - offset
+            val bottom = bounds.bottom - offset
+            framePath.moveTo(left, max(bounds.exactCenterY(), top + radius))
+            framePath.lineTo(left, top + radius)
+            framePath.cubicTo(
+                left, top + radiusHalf,
+                left + radiusHalf, top,
+                left + radius, top
+            )
+            framePath.lineTo(max(left + radius, bounds.exactCenterX()), top)
+
+            framePath.moveTo(right, min(bottom - radius, bounds.exactCenterY()))
+            framePath.lineTo(right, bottom - radius)
+            framePath.cubicTo(
+                right, bottom - radiusHalf,
+                right - radiusHalf, bottom,
+                right - radius, bottom
+            )
+            framePath.lineTo(min(bounds.exactCenterX(), right - radius), bottom)
         }
 
         override fun draw(canvas: Canvas) {
-            TODO("Not yet implemented")
+            canvas.drawPath(framePath, paint)
         }
 
         override fun setAlpha(alpha: Int) {
-            TODO("Not yet implemented")
+            paint.alpha = alpha
         }
 
         override fun setColorFilter(colorFilter: ColorFilter?) {
-            TODO("Not yet implemented")
+            paint.colorFilter = colorFilter
         }
 
         override fun getOpacity(): Int {
