abae853d382611102b7f4cb571a577734eb3dc27
lollipop <liujian594@hotmail.com>
2022-11-15 23:50:36 +0800

    feat: 增加悬浮窗的窗口

--- app/src/main/java/com/lollipop/qin1sptools/floating/FloatingPanel.kt
+++ app/src/main/java/com/lollipop/qin1sptools/floating/FloatingPanel.kt
@@ -1,12 +1,23 @@
 package com.lollipop.qin1sptools.floating
 
 import android.content.Context
+import android.graphics.PixelFormat
+import android.view.LayoutInflater
 import android.view.View
+import android.view.WindowManager
+import com.lollipop.qin1sptools.databinding.FloatingRootBinding
 import com.lollipop.qin1sptools.event.KeyEvent
+import com.lollipop.qin1sptools.event.KeyEvent.*
 import com.lollipop.qin1sptools.event.KeyEventListener
 import com.lollipop.qin1sptools.event.KeyEventProviderHelper
+import com.lollipop.qin1sptools.utils.bind
+import kotlin.math.abs
 
 class FloatingPanel(private val context: Context) : KeyEventListener, View.OnKeyListener {
+
+    companion object {
+        private const val PANEL_OPEN_DURATION = 100L
+    }
 
     private val keyEventProviderHelper by lazy {
         KeyEventProviderHelper(repeatGroup = null, selfListener = this)
@@ -14,16 +25,140 @@ class FloatingPanel(private val context: Context) : KeyEventListener, View.OnKey
 
     private val actionPanelList = ArrayList<FloatingAction>()
 
-    fun onCreate() {
-
+    private val binding: FloatingRootBinding by lazy {
+        LayoutInflater.from(context).bind()
     }
 
-    init {
+    private var starTime = 0L
+    private var poundTime = 0L
 
+    private val animationHelper by lazy {
+        FloatingPanelAnimationHelper(
+            binding.floatingCardView,
+            binding.backgroundView,
+            binding.floatingBottomLineView
+        )
+    }
+
+    fun onCreate() {
+        val wm = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val flags = WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+            .or(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+            .or(WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN)
+            .or(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        val layoutParams = WindowManager.LayoutParams(
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+            flags,
+            PixelFormat.TRANSPARENT
+        )
+        wm.addView(bindKeyListener(binding.root), layoutParams)
+        animationHelper.close(false)
+    }
+
+    private fun bindKeyListener(view: View): View {
+        view.setOnKeyListener(this)
+        view.isFocusableInTouchMode = true
+        return view
     }
 
     override fun onKeyDown(event: KeyEvent, repeatCount: Int): Boolean {
+        if (animationHelper.isOpened) {
+            return invokeByOpened(event)
+        }
+        if (animationHelper.isClosed) {
+            return invokeByClosed(event)
+        }
         return false
+    }
+
+    private fun invokeByClosed(event: KeyEvent): Boolean {
+        val now = System.currentTimeMillis()
+        when (event) {
+            KEY_STAR -> {
+                starTime = now
+            }
+            KEY_POUND -> {
+                poundTime = now
+            }
+            else -> {
+                starTime = 0L
+                poundTime = 0L
+                return false
+            }
+        }
+        if (abs(now - starTime) < PANEL_OPEN_DURATION && abs(now - poundTime) < PANEL_OPEN_DURATION) {
+            starTime = 0L
+            poundTime = 0L
+            openPanel()
+            return true
+        }
+        return false
+    }
+
+    private fun openPanel() {
+        animationHelper.open(true)
+    }
+
+    private fun closePanel() {
+        animationHelper.close(true)
+    }
+
+    private fun lastPage() {
+        // TODO
+    }
+
+    private fun nextPage() {
+        // TODO
+    }
+
+    private fun onPageOptionClick(position: Int) {
+        // TODO
+    }
+
+    private fun invokeByOpened(event: KeyEvent): Boolean {
+        when (event) {
+            KEY_STAR, LEFT -> {
+                lastPage()
+            }
+            KEY_POUND, RIGHT -> {
+                nextPage()
+            }
+            KEY_0, BACK -> {
+                closePanel()
+            }
+            KEY_1 -> {
+                onPageOptionClick(0)
+            }
+            KEY_2 -> {
+                onPageOptionClick(1)
+            }
+            KEY_3 -> {
+                onPageOptionClick(2)
+            }
+            KEY_4 -> {
+                onPageOptionClick(3)
+            }
+            KEY_5 -> {
+                onPageOptionClick(4)
+            }
+            KEY_6 -> {
+                onPageOptionClick(5)
+            }
+            KEY_7 -> {
+                onPageOptionClick(6)
+            }
+            KEY_8 -> {
+                onPageOptionClick(7)
+            }
+            KEY_9 -> {
+                onPageOptionClick(8)
+            }
+            else -> {
+            }
+        }
+        return true
     }
 
     override fun onKey(v: View?, keyCode: Int, event: android.view.KeyEvent?): Boolean {
---
+++ app/src/main/java/com/lollipop/qin1sptools/floating/FloatingPanelAnimationHelper.kt
@@ -0,0 +1,126 @@
+package com.lollipop.qin1sptools.floating
+
+import android.animation.Animator
+import android.animation.ValueAnimator
+import android.view.View
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
+import kotlin.math.abs
+
+class FloatingPanelAnimationHelper(
+    private val bottomSheetPanel: View,
+    private val backgroundView: View,
+    private val floatingHintView: View
+) : ValueAnimator.AnimatorUpdateListener, Animator.AnimatorListener {
+
+    companion object {
+        private const val DURATION = 200L
+
+        private const val PROGRESS_OPENED = 1F
+        private const val PROGRESS_CLOSED = 0F
+
+        private const val THRESHOLD_OPENED = 0.998F
+
+        private const val THRESHOLD_CLOSED = 0.002F
+    }
+
+    private var progress = 0F
+
+    private val valueAnimator = ValueAnimator().apply {
+        addUpdateListener(this@FloatingPanelAnimationHelper)
+        addListener(this@FloatingPanelAnimationHelper)
+    }
+
+    val isOpened: Boolean
+        get() {
+            return progress > THRESHOLD_OPENED
+        }
+
+    val isClosed: Boolean
+        get() {
+            return progress < THRESHOLD_CLOSED
+        }
+
+    fun open(animation: Boolean = true) {
+        doAnimation(true, animation)
+    }
+
+    fun close(animation: Boolean = true) {
+        doAnimation(false, animation)
+    }
+
+    private fun doAnimation(isOpen: Boolean, animation: Boolean) {
+        valueAnimator.cancel()
+        val end = if (isOpen) {
+            PROGRESS_OPENED
+        } else {
+            PROGRESS_CLOSED
+        }
+        if (!animation) {
+            progress = end
+            onProgressChanged()
+            onAnimationEnd()
+            return
+        }
+        valueAnimator.duration = getDuration(end)
+        valueAnimator.setFloatValues(progress, end)
+        valueAnimator.start()
+    }
+
+    private fun getDuration(end: Float): Long {
+        val offset = abs(progress - end) * 1F
+        val max = abs(PROGRESS_OPENED - PROGRESS_CLOSED)
+        return (offset / max * DURATION).toLong()
+    }
+
+    private fun onProgressChanged() {
+        val pro = progress
+        backgroundView.alpha = pro
+        bottomSheetPanel.translationY = (1 - pro) * bottomSheetPanel.height
+        floatingHintView.scaleX = (1 - pro)
+    }
+
+    private fun onAnimationStart() {
+        bottomSheetPanel.isVisible = true
+        backgroundView.isVisible = true
+        floatingHintView.isVisible = true
+    }
+
+    private fun onAnimationEnd() {
+        // 结束的时候，需要把不必要的View隐藏了
+        if (progress < THRESHOLD_CLOSED) {
+            bottomSheetPanel.isInvisible = true
+            backgroundView.isInvisible = true
+            floatingHintView.isVisible = true
+        }
+    }
+
+    override fun onAnimationUpdate(animation: ValueAnimator) {
+        if (animation === valueAnimator) {
+            val animatedValue = animation.animatedValue
+            if (animatedValue is Float) {
+                progress = animatedValue
+                onProgressChanged()
+            }
+        }
+    }
+
+    override fun onAnimationStart(animation: Animator) {
+        if (animation === valueAnimator) {
+            onAnimationStart()
+        }
+    }
+
+    override fun onAnimationEnd(animation: Animator) {
+        if (animation === valueAnimator) {
+            onAnimationEnd()
+        }
+    }
+
+    override fun onAnimationCancel(animation: Animator) {
+    }
+
+    override fun onAnimationRepeat(animation: Animator) {
+    }
+
+}
