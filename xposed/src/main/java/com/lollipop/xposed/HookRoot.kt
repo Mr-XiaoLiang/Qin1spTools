3f53e89cd29f502e1a9804740153fb31c0b3ad72
lollipop <liujian594@hotmail.com>
2022-11-17 22:14:16 +0800

    feat: 为什么编译不过呢？

---
+++ xposed/src/main/java/com/lollipop/xposed/HookRoot.kt
@@ -0,0 +1,11 @@
+package com.lollipop.xposed
+
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+class HookRoot: IXposedHookLoadPackage {
+
+    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam?) {
+    }
+
+}
